60233b0d506bdba5448b02824000ded4d5bccb57
ywindmaple <ywindmaple@gmail.com>
2021-09-23 16:33:49 +0800

    Update integer_convert_bit_or_logic.py
    add description and double loop version, fix output

--- Study_group/Week1/integer_convert_bit_or_logic.py
+++ Study_group/Week1/integer_convert_bit_or_logic.py
@@ -1,26 +1,43 @@
-input_list1 = [8, 13, 2, 19, 27]
-input_list2 = [23, 7, 14, 26, 2]
+"""Problem Description:
+input
+    arr1:
+        [9, 20, 28, 18, 11]
+    arr2:
+        [30, 1, 21, 17, 28]
 
-print(input_list1)
+output
+    ["#####","# # #", "### #", "# ##", "#####"]
 
-graph_output = [format((int(input_list1[i]) | int(input_list2[i])),'05b').replace('1','#').replace('0',' ') for i in range(5)]
+"""
 
-print(graph_output)
+# One Loop Version
+print("One Loop Version :")
+
+input_list1 = [9, 20, 28, 18, 11]
+input_list2 = [30, 1, 21, 17, 28]
+
+graph_output_one_loop = [format((int(input_list1[i]) | int(input_list2[i])),'05b').replace('1','#').replace('0',' ') for i in range(5)]
+
+print(graph_output_one_loop)
 
 
-# bit_list1 = [format(i, '05b') for i in input_list1]
-# bit_list2 = [format(i, '05b') for i in input_list2]
-# print (bit_list1)
-# print (bit_list2)
-# output_list = []
-# for i in range(5):
-#     num1 = bit_list1[i]
-#     num2 = bit_list2[i]
-#     c = [int(num1[j])|int(num2[j])for j in range(5)]
-#     res = 0
-#     for j in range(5):
-#         if c[j] == 1:
-#             res += 1 <<(4-j)
-#     output_list.append(res)
+# Double For Loop Version
+print("Double Loop Version :")
 
-# print(output_list)
+bit_list1 = [format(i, '05b') for i in input_list1]
+bit_list2 = [format(i, '05b') for i in input_list2]
+graph_out_double_loop = []
+for i in range(5):
+    num1 = bit_list1[i]
+    num2 = bit_list2[i]
+    c = [int(num1[j])|int(num2[j])for j in range(5)]  
+
+    res_str = ''
+    for k in range(5):
+        if c[k] == 1:
+            res_str += '#'
+        elif c[k] == 0:
+            res_str += ' '
+    graph_out_double_loop.append(res_str)
+
+print(graph_out_double_loop)
